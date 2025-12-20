a461b1771c1443b07ce871d6a6a17bf100dbcc84
Sayak Chowdhury <sayakchowdhury384@gmail.com>
2025-12-20 10:21:16 +0530

    Quartz sync: Dec 20, 2025, 10:21 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠 Electric Brains",
+    pageTitle: "sykchw",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
